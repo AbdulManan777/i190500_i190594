4cecf5be65bc27b7164f519e5f0eed8c3d57bfee
umarmansoor001 <i190594@nu.edu.pk>
2022-10-18 00:40:56 +0500

    Musify logo screen Time delay implemented

--- app/src/main/java/com/ass2/i190500_i190594/MainActivity.java
+++ app/src/main/java/com/ass2/i190500_i190594/MainActivity.java
@@ -2,7 +2,13 @@ package com.ass2.i190500_i190594;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onResume(){
+        super.onResume();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(MainActivity.this, sign_up.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 3000);// Musify logo screen will be delayed 3 seconds
+    }
+
 }
